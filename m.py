fdb72cfbeb4a5ea0120f362dfd9502ae57a233f9
Mmadiba724 <matendehussein@gmail.com>
2024-07-11 22:53:47 +0300

    madiba

--- m.py
+++ m.py
@@ -1,33 +1,47 @@
-from typing import Protocol
+from abc import ABC, abstractmethod
 
-class InputDevice(Protocol):
+class InputDevice(ABC):
+
+    @abstractmethod
     def input(self):
         pass
 
-class KeyBoard:
+class KeyBoard(InputDevice):
 
     def input(self):
         print("Inputting data from a keyboard...")
 
 
-class Mouse:
+class Mouse(InputDevice):
 
     def input(self):
         print("Inputting data from a mouse...")
 
 
 class Computer:
-    def __init__(self):
+    name: str =""
+    # __input_device:InputDevice
+    
+    def __init__(self, input_device:InputDevice):
         self.name: str
-        self.__input_device = InputDevice()
+        self.__input_device = input_device
+    
+    def input(self):
+        self.__input_device.input()
 
     
 
     
 
-computer: Computer = Computer()
+keyboard = KeyBoard()
+mouse = Mouse()
+computer_using_keyboard = Computer(keyboard).input()
+print("============================================================")
+computer_using_mouse = Computer(mouse).input()
 
-computer.input_device
+# computer: Computer = Computer()
+
+# computer.input_device
 # computer.input_data_from_keyboard()
 # computer.store_data_to_internal_memory()
 # computer.retrieve_data_to_internal_memory()
